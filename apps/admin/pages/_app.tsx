223d6297e5cf42022c370892274f4c748a08c101
leobar37 <leobar37>
2021-10-29 22:13:22 -0500

    initalize repo

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,19 +1,9 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to admin!</title>
-      </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to admin!</h1>
-        </header>
         <main>
           <Component {...pageProps} />
         </main>
