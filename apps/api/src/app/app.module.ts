f0eecb7d82d0fe3ccc7971898f4fdbb7f2136c4f
leobar37 <leobar37>
2021-11-02 11:27:08 -0500

    chore : script make entities added

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      password: 'alfk3458',
+      username: 'leobar37',
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ libs/common/src/shared-types.ts
@@ -0,0 +1,17 @@
+export type ID = string | number;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type SafeAny = any;
+
+/**
+ * A recursive implementation of the Partial<T> type.
+ * Source: https://stackoverflow.com/a/49936686/772859
+ */
+export type DeepPartial<T> = {
+  [P in keyof T]?:
+    | null
+    | (T[P] extends Array<infer U>
+        ? Array<DeepPartial<U>>
+        : T[P] extends ReadonlyArray<infer U>
+        ? ReadonlyArray<DeepPartial<U>>
+        : DeepPartial<T[P]>);
+};
