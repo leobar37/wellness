2d5cb4bbc43eb83464fb61edc8a56987435bc893
leobar37 <leobar37>
2021-11-08 00:11:05 -0500

    chore (core): add logic in contractsScheduler

--- libs/core/src/scheduler/Scheduler.ts
+++ libs/core/src/scheduler/Scheduler.ts
@@ -6,30 +6,33 @@ import { Type } from '@nestjs/common';
 import { SafeAny } from '@wellness/common';
 
 export abstract class Scheduler implements OnModuleDestroy {
-  private destroy$ = new Subject<void>();
+  protected destroy$ = new Subject<void>();
   notifierStream$ = new Subject<Task>();
 
-  taks: Task[] = [];
-  // this method is called when application is started
-  // it is useful if you want prepare tasks
-  // when the application start
-  abstract onInit(): void;
+  register = new Map<Task, SafeAny>();
 
   onModuleDestroy(): SafeAny {
     this.destroy$.next();
     this.destroy$.complete();
   }
+
   protected onTasks(tasks: Task[]) {
     for (const task of tasks) {
       this.onTask(task);
     }
   }
 
+  cancelTask(task: Task) {
+    const callback = this.register.get(task);
+    scheduler.cancelJob(callback);
+  }
+
   protected onTask(task: Task) {
-    this.taks.push(task);
-    scheduler.scheduleJob(task.endDate, () => {
+    const callback = () => {
       this.notifierStream$.next(task);
-    });
+    };
+    this.register.set(task, callback);
+    scheduler.scheduleJob(task.endDate, callback);
   }
 
   ofType<T extends Task>(type: Type<T>): Observable<T> {
--- libs/core/src/scheduler/contracts/ContractScheduler.ts
+++ libs/core/src/scheduler/contracts/ContractScheduler.ts
@@ -2,10 +2,71 @@ import { Scheduler } from '../Scheduler';
 import { Contract, Suscription } from '@wellness/core/entity';
 import { DinamicTask, FixedTask } from './task';
 import { STATETASK } from '../Task';
-class ContractsScheduler extends Scheduler {
-  onInit(): void {
-    // super.onTasks();
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { ModeSuscription } from '@wellness/common';
+import { takeUntil } from 'rxjs/operators';
+@Injectable()
+export class ContractsScheduler extends Scheduler implements OnModuleInit {
+  constructor(@InjectEntityManager() private manager: EntityManager) {
+    super();
   }
+  onModuleInit() {
+    this.initializeTasks();
+    this.initListenEvents();
+  }
+
+  private initListenEvents() {
+    this.ofType(DinamicTask)
+      .pipe(takeUntil(super.destroy$))
+      .subscribe(async (task) => {
+        await this.manager.update(Contract, task.contractId, {
+          finished: true,
+        });
+      });
+    this.ofType(FixedTask)
+      .pipe(takeUntil(super.destroy$))
+      .subscribe(async (task) => {
+        // TODO: Use a transaction here ->
+        const sub = await this.manager.findOne(Suscription, {
+          where: {
+            id: task.suscriptionId,
+          },
+        });
+        // desactive subscription
+        this.manager.update(Suscription, sub.id, {
+          active: false,
+        });
+
+        const contracts = await sub.contracts;
+        for (const contract of contracts) {
+          await this.manager.update(Contract, contract.id, {
+            finished: true,
+          });
+          // TODO:
+          // - lauch a notification here
+        }
+      });
+  }
+
+  private async initializeTasks() {
+    const suscriptions = await this.manager.find(Suscription, {});
+    suscriptions.forEach(async (sub) => {
+      const isFixed = sub.mode == ModeSuscription.FIXED;
+      if (isFixed) {
+        this.addFixedTask(sub);
+      } else {
+        const contracts = await sub.contracts;
+        for (const contract of contracts) {
+          if (!contract.finished) {
+            this.addDinamicTask(contract);
+          }
+        }
+      }
+    });
+  }
+
   // this dinamic
   addDinamicTask(contract: Contract) {
     this.onTask(
