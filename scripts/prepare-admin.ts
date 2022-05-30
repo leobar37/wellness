cc624f3f3f5aa3c06f731ae4d50d0034b6cd61ce
leobar37 <leonardito3459@gmail.com>
2022-05-29 20:12:04 -0500

    chore: update action

--- scripts/prepare-admin.ts
+++ scripts/prepare-admin.ts
@@ -1,8 +1,14 @@
 // copy env
-import { exec, runTask } from './util';
+import { exec, cmd, runTask } from './util';
 
 const main = async () => {
   await exec('nx', ['build', 'admin', '--production']);
+  await cmd(
+    'cd dist/apps/admin && docker build -f ../../../docker/Dockerfile.wellfront -t leobar37/wellfront:latest .',
+    []
+  );
+  await cmd('dokku git:from-image wellfront leobar37/wellfront:latest', []);
+  await cmd('dokku ps:rebuild wellfront', []);
 };
 
-runTask('Prepare api project', main);
+runTask('Prepare admin project', main);
